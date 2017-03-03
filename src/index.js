b62eda3eb75b8b67f4bee3f10a977d6a2f073ad1
John Shaw <shaw_fire@yahoo.com>
2017-03-04 05:55:42 +1100

    JSX explained.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 // Create a new compoent.
 //  This component should produce some HTML.
 const App = function() {
-  return <div>Hi!</div>;  // JSX - What looks like HTML in our JavaScript
+  // JSX - What looks like HTML in our JavaScript
+  //  this code gets transpiled with babel to produce JS
+  //  i.e. babeljs.io/repl
+  return <div>Hi!</div>;
+  // the above jsx converts to using babel:
+  //   return React.createElement(
+  //    "div",
+  //    null,
+  //    "Hi!"
+  // );
 }
+
+// Note: Cmd Alt i - Opens up console window in Chrome
+
 // Take this component's generated HTML and put it on the page (in the DOM)
