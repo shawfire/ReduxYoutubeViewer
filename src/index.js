311d99d4029ccb33ee99e719d02358516a9b2fc4
John Shaw <shaw_fire@yahoo.com>
2017-03-04 06:03:13 +1100

    JSX further explained.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,31 @@ const App = function() {
   // JSX - What looks like HTML in our JavaScript
   //  this code gets transpiled with babel to produce JS
   //  i.e. babeljs.io/repl
-  return <div>Hi!</div>;
+  return <ol>
+    <li>JavaScript</li>
+    <li>JSX</li>
+    <li>Babel</li>
+    </ol>;
   // the above jsx converts to using babel:
-  //   return React.createElement(
-  //    "div",
-  //    null,
-  //    "Hi!"
-  // );
-}
+  //  (thus the purpose of JSX is to make your HTML legible within JS.
+  // return React.createElement(
+  //     "ol",
+  //     null,
+  //     React.createElement(
+  //       "li",
+  //       null,
+  //       "JavaScript"
+  //     ),
+  //     React.createElement(
+  //       "li",
+  //       null,
+  //       "JSX"
+  //     ),
+  //     React.createElement(
+  //       "li",
+  //       null,
+  //       "Babel"
+  //     )}
 
 // Note: Cmd Alt i - Opens up console window in Chrome
 
