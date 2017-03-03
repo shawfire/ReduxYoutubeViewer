adf340efa215263a70b99a3914e43c3c109d7f85
John Shaw <shaw_fire@yahoo.com>
2017-03-04 10:02:56 +1100

    Adding search bar.

---
+++ src/components/search_bar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// //functional component with out state
+// const SeachBar = () => {
+//   return <input />;
+// };
+
+// ES6 class - state
+class SearchBar extend React.Component {
+  render() {
+    return <input />;
+  }
+}
+
+export default SeachBar;
