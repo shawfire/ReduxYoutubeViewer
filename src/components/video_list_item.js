724be88cef750e50c4db2353ac2efce3d55534ef
John Shaw <shaw_fire@yahoo.com>
2017-03-04 16:16:39 +1100

    Display youtube thumbnail.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 const VideoListItem = ({video}) => {  // ES6 syntaxs
   // const video = props.video;
+  const imageUrl = video.snippet.thumbnails.default.url;
   return (
-    <li>
-      {video.snippet.title}
+    <li className="list-group-item">
+      <div className="video-list media">
+        <div className= "media-left">
+          <img className="media-object" src={imageUrl} />
+        </div>
+        <div className="media-body">
+          <div className="media-heading">{video.snippet.title}</div>
+        </div>
+      </div>
     </li>
   );
 };
